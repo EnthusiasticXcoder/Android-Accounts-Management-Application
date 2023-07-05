600e891e12adc29bb75257519af814b257335d14
Anshul Verma <anshulverma1405@gmail.com>
2023-07-06 02:47:39 +0530

    add height and width arguments

--- lib/utilities/generics/get_argument.dart
+++ lib/utilities/generics/get_argument.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart' show BuildContext, MediaQuery;
 
 extension GetArgument on BuildContext {
-
   double getminheight() {
     final mediaquery = MediaQuery.of(this);
     final args = mediaquery.size.height * 0.55;
@@ -12,5 +11,15 @@ extension GetArgument on BuildContext {
     final mediaquery = MediaQuery.of(this);
     final args = mediaquery.size.height * 0.7;
     return args;
+  }
+
+  double get height {
+    final mediaquery = MediaQuery.of(this);
+    return mediaquery.size.height;
+  }
+
+  double get width {
+    final mediaquery = MediaQuery.of(this);
+    return mediaquery.size.width;
   }
 }
