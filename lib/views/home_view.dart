a686fb91014fe9230626c6a0ad27c9fcb0dec367
Anshul Verma <anshulverma1405@gmail.com>
2023-07-06 02:53:16 +0530

    minor changes and code cleanup

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/views/widgets/head_bar_widget.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-import 'package:my_app/utilities/vertical_controller.dart';
+import 'package:my_app/utilities/controllers/vertical_controller.dart';
 import 'package:my_app/views/widgets/balance_notation_widget.dart';
 import 'package:my_app/views/widgets/tab_bar_widget.dart';
 
@@ -22,6 +23,7 @@ class _MyHomeViewState extends State<MyHomeView> {
   @override
   void initState() {
     _databaseService = DatabaseService();
+    _databaseService.open();
     super.initState();
   }
 
@@ -33,54 +35,39 @@ class _MyHomeViewState extends State<MyHomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _databaseService.open(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            return Scaffold(
-              backgroundColor: Colors.blue.shade500,
-              body: Stack(
-                children: <Widget>[
-                  // Header Widget
-                  _headerwidget(context),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.blue.shade500,
+      body: Stack(
+        children: <Widget>[
+          // Header Widget
+          const Headwidget(),
 
-                  // Bottom Sliding Pannel
-                  SlidingUpPanel(
-                    body: const BalanceNotationWidget(),
-                    parallaxEnabled: true,
-                    parallaxOffset: 0.75,
-                    panelBuilder: (sc) {
-                      VerticalController().setcontroller(sc);
-                      return const TabbarWidget();
-                    },
-                    borderRadius:
-                        const BorderRadius.vertical(top: Radius.circular(25)),
-                    minHeight: context.getminheight(),
-                    maxHeight: context.getmaxheight(),
-                  ),
-                ],
-              ),
-            );
-          default:
-            return const CircularProgressIndicator();
-        }
-      },
+          // Bottom Sliding Pannel
+          SlidingUpPanel(
+            body: FutureBuilder(
+              future: _databaseService.open(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.done:
+                    return const BalanceNotationWidget();
+                  default:
+                    return const BalanceNotationWidget();
+                }
+              },
+            ),
+            parallaxEnabled: true,
+            parallaxOffset: 0.75,
+            panelBuilder: (sc) {
+              VerticalController().setcontroller(sc);
+              return const TabbarWidget();
+            },
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+            minHeight: context.getminheight(),
+            maxHeight: context.getmaxheight(),
+          ),
+        ],
+      ),
     );
   }
-
-  Widget _headerwidget(BuildContext context) => const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(25.0),
-          child: Text(
-            'Hi, Anshul!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      );
 }
-
