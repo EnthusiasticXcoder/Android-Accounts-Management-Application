1d8293e53c468638c9397856856843d9371ddc63
Anshul Verma <anshulverma1405@gmail.com>
2023-07-06 02:52:51 +0530

    add animated digits

--- lib/views/widgets/balance_notation_widget.dart
+++ lib/views/widgets/balance_notation_widget.dart
@@ -1,7 +1,9 @@
+import 'package:animated_digit/animated_digit.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/constants/crud_constants.dart';
 
 import 'package:my_app/services/database_service.dart';
-import 'package:my_app/utilities/get_state.dart';
+import 'package:my_app/utilities/controllers/state_controller.dart';
 
 class BalanceNotationWidget extends StatefulWidget {
   const BalanceNotationWidget({super.key});
@@ -11,51 +13,98 @@ class BalanceNotationWidget extends StatefulWidget {
 }
 
 class _BalanceNotationWidgetState extends State<BalanceNotationWidget> {
+  late final DatabaseService _databaseService;
+
+  @override
+  void initState() {
+    _databaseService = DatabaseService();
+    StateController().addStateFunction(setState);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var balance = DatabaseService().getBalance;
-    int amount = balance[0];
-    var color = balance[1];
-    BalanceState().setFunction(setState);
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         // Upper Margin
         const SizedBox(),
+        // Total Income and expense widgets
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Income Indecator
+            _balanceCard(
+              context,
+              amount: _databaseService.gettotalIncome,
+              icon: Icons.arrow_downward_rounded,
+              color: Colors.green,
+            ),
 
+            // Expense Indecator
+            _balanceCard(
+              context,
+              amount: _databaseService.getTotalExpense,
+              icon: Icons.arrow_upward_rounded,
+              color: Colors.red,
+            ),
+          ],
+        ),
         // Balance Field
+        AnimatedDigitWidget(
+          prefix: '₹',
+          value: _databaseService.getBalance,
+          enableSeparator: true,
+          textStyle: TextStyle(
+            color: _databaseService.diffColor,
+            fontSize: 76,
+            fontWeight: FontWeight.bold,
+            height: 0,
+          ),
+        ),
+
+        // Balance Notation
         Text(
-          '₹ $amount',
+          totalBalance,
           textHeightBehavior: const TextHeightBehavior(
             applyHeightToFirstAscent: false,
             applyHeightToLastDescent: false,
           ),
           style: TextStyle(
-            color: color,
-            fontSize: 76,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-
-        // Balance Notation
-        const Text(
-          totalBalance,
-          textHeightBehavior: TextHeightBehavior(
-            applyHeightToFirstAscent: false,
-            applyHeightToLastDescent: false,
-          ),
-          style: TextStyle(
-            color: Colors.white,
+            color: Colors.blueGrey.shade100,
             fontSize: 18,
           ),
         ),
 
         // Lower Margin
-        const SizedBox(height: 350),
+        const SizedBox(height: 300),
       ],
     );
   }
-}
 
-const totalBalance = 'TOTAL BALANCE';
+  Widget _balanceCard(BuildContext context,
+          {required int amount,
+          required IconData icon,
+          required Color color}) =>
+      Card(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                icon,
+                color: color,
+              ),
+              // margin
+              const SizedBox(width: 2),
+              AnimatedDigitWidget(
+                prefix: '₹',
+                value: amount,
+                textStyle: TextStyle(color: color, height: 0),
+              )
+            ],
+          ),
+        ),
+      );
+}
