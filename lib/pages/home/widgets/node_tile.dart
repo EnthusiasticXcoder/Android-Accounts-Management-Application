0629745587dfb4f1d42bfd5dd8b7f62dddecc651
Anshul Verma <anshulverma1405@gmail.com>
2023-07-15 16:31:13 +0530

    added nodetile, clean code

---
+++ lib/pages/home/widgets/node_tile.dart
@@ -0,0 +1,74 @@
+part of 'tab_bar.dart';
+
+class NodeTile extends StatelessWidget {
+  final bool isIncome;
+  final String description;
+  final String dateTime;
+  final int amount;
+
+  const NodeTile({
+    super.key,
+    required this.isIncome,
+    required this.description,
+    required this.dateTime,
+    required this.amount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 4,
+      ),
+      child: ListTile(
+        // Leading Icon
+        leading: Icon(
+          isIncome ? Icons.arrow_downward_rounded : Icons.arrow_upward_rounded,
+          color: isIncome ? Colors.green : Colors.red,
+          size: 35,
+        ),
+
+        // Description
+        title: Text(
+          description,
+          style: const TextStyle(
+            overflow: TextOverflow.ellipsis,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+
+        // Date and Time
+        subtitle: Text(
+          dateTime,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 12,
+          ),
+        ),
+        // Value Amount
+        trailing: Text(
+          '₹$amount',
+          style: TextStyle(
+            color: isIncome ? Colors.green : Colors.red,
+            fontSize: 22,
+          ),
+        ),
+
+        // Discription overview
+        onLongPress: () {
+          showDialog(
+            context: context,
+            builder: (context) => DisplayMoreDialog(
+              amount: amount.toString(),
+              dateTime: dateTime,
+              description: description,
+              statusColor: isIncome ? Colors.green : Colors.red,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/widgets/tab_list.dart
@@ -0,0 +1,52 @@
+part of 'tab_bar.dart';
+
+
+class TabListView extends StatelessWidget {
+  final bool isIncome;
+  late final DatabaseService _service;
+
+  TabListView({
+    super.key,
+    required this.isIncome,
+  }) {
+    _service = DatabaseService();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _service.getstream(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+          case ConnectionState.active:
+            final nodes = _service.getNodes(statuscolumn, isIncome);
+            return ListView.builder(
+              controller: VerticalController().getcontroller,
+              itemCount: nodes.length,
+              itemBuilder: (context, index) {
+                final node = nodes.elementAt(index);
+                int amount = node.amount;
+                String description = node.description;
+                String dateTime =
+                    '${node.date}/${node.month}/${node.year} \t ${node.hour}:${node.minutes}';
+                    
+                return NodeTile(
+                  isIncome: isIncome,
+                  description: description,
+                  dateTime: dateTime,
+                  amount: amount,
+                );
+              },
+            );
+
+          default:
+            return ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) =>
+                    LoadingTile(isincome: isIncome));
+        }
+      },
+    );
+  }
+}
