75ac0626dee58df1d8ccc7225fd970fba54b5bb1
Anshul Verma <anshulverma1405@gmail.com>
2023-08-15 22:19:34 +0530

    added conformation before delete user

--- lib/pages/settings/view/profile.dart
+++ lib/pages/settings/view/profile.dart
@@ -38,12 +38,44 @@ class ProfilePage extends StatelessWidget {
           backgroundColor: Colors.redAccent,
           child: const Icon(Icons.delete_forever),
           onPressed: () {
-            // Deleting Current Active User
-            final userId = activeuser.id;
-            context.read<NodeBloc>().add(NodeEventDeleteUser(userId));
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              AppRouts.homepage,
-              (route) => route.settings.name == AppRouts.homepage,
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text(
+                  'Delete User',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.redAccent,
+                  ),
+                ),
+                content: const Text('Are You Sure Want To Delete Account?'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancle'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Deleting Current Active User
+                      final userId = activeuser.id;
+                      context.read<NodeBloc>().add(NodeEventDeleteUser(userId));
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        AppRouts.homepage,
+                        (route) => route.settings.name == AppRouts.homepage,
+                      );
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'Delete',
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           },
         ),
